7a60b18c16497083daf0eec0e31a1e7b8690f55a
Peter Halliday <pghalliday@gmail.com>
2015-11-23 00:45:54 +0100

    proxy configuration

--- providers/proxy.rb
+++ providers/proxy.rb
@@ -5,13 +5,12 @@ end
 use_inline_resources
 
 action :create do
-  template "#{node['apache']['dir']}/sites-available/#{new_resource.hostname}" do
+  template "#{node['apache']['dir']}/sites-available/#{new_resource.hostname}.conf" do
     cookbook 'formatron_apache'
     source 'proxy.erb'
     variables(
       hostname: "#{new_resource.hostname}",
       port: new_resource.port,
-      log_directory: node['apache']['log_dir'],
       ldap: {
         auth_name: new_resource.ldap_auth_name,
         url: new_resource.ldap_url,
@@ -20,5 +19,4 @@ action :create do
       }
     )
   end
-  apache_site new_resource.hostname
 end
