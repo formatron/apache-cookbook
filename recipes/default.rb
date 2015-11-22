5df1ae2d401f78ae43222dae74b8e440c10320d7
Peter Halliday <pghalliday@gmail.com>
2015-11-22 14:58:24 +0100

    base apache install

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,6 @@
+node.override['apache']['default_site_enabled'] = false
+
 include_recipe 'apache2::default'
 include_recipe 'apache2::mod_proxy'
 include_recipe 'apache2::mod_proxy_http'
-include_recipe 'apache2::mod_ssl'
-
-package 'libapache2-mod-authnz-external' do
-  notifies :restart, 'service[apache2]', :delayed
-end
-
-execute "sudo a2enmod authn_socache"
+include_recipe 'apache2::mod_authnz_ldap'
