ad5ee660d556188d18072820e97ca51176c7120d
Peter Halliday <pghalliday@gmail.com>
2015-12-10 14:55:45 +0100

    fancy new publish script

--- publish.rb
+++ publish.rb
@@ -7,6 +7,14 @@ category = 'Web Servers'
 metadata = Chef::Cookbook::Metadata.new
 metadata.from_file './metadata.rb'
 
-`git tag v#{metadata.version}`
-`git push --tags`
-`chef exec knife cookbook site share '#{metadata.name}' "#{category}"`
+def run(cmd, fail_message)
+  system cmd
+  fail fail_message unless $? == 0
+end
+
+run 'git diff --exit-code > /dev/null', 'Failed: there are local unstaged changes'
+run 'git diff --cached --exit-code > /dev/null', 'Failed: there are local staged but uncommitted changes'
+run 'git push', 'Failed: could not push local commits'
+run "git tag v#{metadata.version}", "Failed: could not create tag v#{metadata.version}"
+run 'git push --tags', "Failed: could not push tag v#{metadata.version}"
+run "chef exec knife cookbook site share '#{metadata.name}' \"#{category}\"", "Failed: could not share cookbook #{metadata.name} with version #{metadata.version} under category #{category}"
